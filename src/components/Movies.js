f4608625aea40e077467c86afc236ee076a2460c
Jama1202 <example@gmail.com>
2022-11-08 14:16:33 +0300

    A project I made without looking at the video

---
+++ src/components/Movies.js
@@ -0,0 +1,29 @@
+import Movie from "./Movie";
+
+export default function Movies(props) {
+  const { movies = [] } = props;
+
+  return (
+    <div className="movies">
+      {movies.length ? movies.map(movie => (
+        <Movie key={movie.imdbID} {...movie} />
+      )) : 
+      <div>
+      <div id="error-page">
+         <div class="content">
+            <h2 class="header" data-text="404">
+               404
+            </h2>
+            <h4 data-text="Opps! Page not found">
+               Opps! Page not found
+            </h4>
+            <p>
+               Sorry, the page you're looking for doesn't exist. 
+            </p>  
+         </div>
+      </div>
+      </div>
+      }
+    </div>
+  );
+}
